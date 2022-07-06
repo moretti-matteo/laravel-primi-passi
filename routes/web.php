7c90a0db3bc0ce5f1a5b078dd2ea426e9a14670e
moretti-matteo <matteomoretti2000@hotmail.it>
2022-07-06 16:01:36 +0200

    bonus

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/contatti', function () {
+    return view('contatti');
+});
+
+Route::get('/info', function () {
+    return view('info');
 });
